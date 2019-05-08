98862b2ece084d3f0cdbf6c712ea969ab9584dbb
Dioni <dioni.rebonato.001@student.uni.lu>
2019-05-08 21:40:05 +0200

    Increasing Dataset

--- spider-v1.6.scala
+++ spider-v1.6.scala
@@ -45,81 +45,73 @@ def processData (allFiles: RDD[java.io.File]) = {
     toFlatData.flatMap(x => x.map(s =>s))
 }
 
+def createFeaturesVector (inputRDD: 
+    RDD[(String, Int, Int, Float, Int, Int, Int, Int, Int, Int, Float, Int, Int, Int, String, Int)]) = {
+        val vector01 = inputRDD.map(x => (LabeledPoint(x._16.toDouble, 
+            Vectors.dense(x._9.toDouble, x._10.toDouble, x._11.toDouble,
+                x._12.toDouble, x._13.toDouble, x._14.toDouble))))
+        val vector02 = inputRDD.map(x => (LabeledPoint(x._16.toDouble, 
+            Vectors.dense(x._2.toDouble, x._3.toDouble, x._4.toDouble,
+                x._5.toDouble, x._6.toDouble, x._7.toDouble, x._8.toDouble,
+                x._9.toDouble, x._10.toDouble, x._11.toDouble,
+                x._12.toDouble, x._13.toDouble, x._14.toDouble))))
+        val vector03 = inputRDD.map(x => (LabeledPoint(x._16.toDouble, 
+            Vectors.dense(x._2.toDouble, x._3.toDouble, x._5.toDouble, 
+                x._6.toDouble, x._9.toDouble, x._10.toDouble,
+                x._12.toDouble, x._13.toDouble))))
+        val vector04 = inputRDD.map(x => (LabeledPoint(x._16.toDouble, 
+            Vectors.dense(x._4.toDouble, x._5.toDouble, 
+                x._6.toDouble, x._8.toDouble, x._11.toDouble, 
+                x._12.toDouble, x._13.toDouble))))
+        val vector05 = inputRDD.map(x => (LabeledPoint(x._16.toDouble, 
+            Vectors.dense(x._2.toDouble, x._3.toDouble, x._4.toDouble,
+                x._5.toDouble, x._6.toDouble, x._7.toDouble, x._8.toDouble,
+                x._9.toDouble, x._11.toDouble, x._12.toDouble,
+                x._13.toDouble, x._14.toDouble))))
+    Array(vector01, vector02, vector03, vector04, vector05)
+}
+
 // Capture the entire path for all files in the current folder ending with "html" extension 
 // It results in an array which will be parallelized to serve as an input in the def processData.
 val allFiles = sc.parallelize(new io.File("./").listFiles.filter(_.getName.endsWith(".html")).map(_.getCanonicalFile))
-val Array(allTrainFiles, allTestFiles) = allFiles.randomSplit(Array(0.5,0.5))
+val Array(allTrainFiles, allTestFiles, allValFiles) = allFiles.randomSplit(Array(0.5,0.3,0.2))
 val allTrainData = processData(allTrainFiles)
 val allTestData = processData(allTestFiles)
-println("\n\n\n>>>>>>> Number Files: " + allFiles.count)
+val allValData = processData(allValFiles)
+println("\n\n\n>>>>>>> Total Files: " + allFiles.count)
+println(">>>>>>> Number Train Files: " + allTrainFiles.count)
+println(">>>>>>> Number Test Files: " + allTestFiles.count)
+println(">>>>>>> Number Val Files: " + allValFiles.count)
 println(">>>>>>> Number Train Lines: " + allTrainData.count)
 println(">>>>>>> Number Test Lines: " + allTestData.count)
-allTrainData.persist()
-allTestData.persist()
-
-val featuresVectorTrain = Array(
-    allTrainData.map(x => (LabeledPoint(x._16.toDouble, 
-        Vectors.dense(x._9.toDouble, x._10.toDouble, x._11.toDouble,
-            x._12.toDouble, x._13.toDouble, x._14.toDouble)))), 
-    allTrainData.map(x => (LabeledPoint(x._16.toDouble, 
-        Vectors.dense(x._2.toDouble, x._3.toDouble, x._4.toDouble,
-            x._5.toDouble, x._6.toDouble, x._7.toDouble, x._8.toDouble,
-            x._9.toDouble, x._10.toDouble, x._11.toDouble,
-            x._12.toDouble, x._13.toDouble, x._14.toDouble)))), 
-    allTrainData.map(x => (LabeledPoint(x._16.toDouble, 
-        Vectors.dense(x._2.toDouble, x._3.toDouble, x._5.toDouble, 
-            x._6.toDouble, x._9.toDouble, x._10.toDouble,
-            x._12.toDouble, x._13.toDouble)))), 
-    allTrainData.map(x => (LabeledPoint(x._16.toDouble, 
-        Vectors.dense(x._4.toDouble, x._5.toDouble, 
-            x._6.toDouble, x._8.toDouble, x._11.toDouble, 
-            x._12.toDouble, x._13.toDouble)))))
-
-val featuresVectorTest = Array(
-    allTestData.map(x => (LabeledPoint(x._16.toDouble, 
-        Vectors.dense(x._9.toDouble, x._10.toDouble, x._11.toDouble,
-            x._12.toDouble, x._13.toDouble, x._14.toDouble)))), 
-    allTestData.map(x => (LabeledPoint(x._16.toDouble, 
-        Vectors.dense(x._2.toDouble, x._3.toDouble, x._4.toDouble,
-            x._5.toDouble, x._6.toDouble, x._7.toDouble, x._8.toDouble,
-            x._9.toDouble, x._10.toDouble, x._11.toDouble,
-            x._12.toDouble, x._13.toDouble, x._14.toDouble)))), 
-    allTestData.map(x => (LabeledPoint(x._16.toDouble, 
-        Vectors.dense(x._2.toDouble, x._3.toDouble, x._5.toDouble, 
-            x._6.toDouble, x._9.toDouble, x._10.toDouble,
-            x._12.toDouble, x._13.toDouble)))), 
-    allTestData.map(x => (LabeledPoint(x._16.toDouble, 
-        Vectors.dense(x._4.toDouble, x._5.toDouble, 
-            x._6.toDouble, x._8.toDouble, x._11.toDouble, 
-            x._12.toDouble, x._13.toDouble)))))
-
-allTrainData.unpersist()
-allTestData.unpersist()
+println(">>>>>>> Number Val Lines: " + allValData.count)
+val featuresVectorTrain = createFeaturesVector(allTrainData)
+val featuresVectorTest = createFeaturesVector(allTestData)
+val featuresVectorVal = createFeaturesVector(allValData)
 
 /// ------------------- Testing Scenarios
-println("Testing Files From: " + new java.io.File(".").getCanonicalPath)
+println("Working on Files From: " + new java.io.File(".").getCanonicalPath)
 for (x <- 0 to featuresVectorTrain.size-1) {
     val trainingData = featuresVectorTrain(x)
     val testData = featuresVectorTest(x)
+    val valData = featuresVectorVal(x)
 
     trainingData.persist()
+    valData.persist()
 
-    val Array(trainDt, valDt) = trainingData.randomSplit(Array(0.8, 0.2))
-    trainDt.cache()
-    valDt.cache()
-
-    val cvData = MLUtils.kFold(trainingData, 10, 0)
+    val k = if (allFiles.count > 100) {7} else {2}
+    val cvData = MLUtils.kFold(trainingData, k, 0)
     val numClasses = 2
     val catFeature = Map[Int, Int]()
 
     // Evaluation to find the best model
     val modelTuningDT = for (
-        (trainDt, valDt) <- cvData; 
+        (trainingData, valData) <- cvData; 
         impurity <- Array("gini", "entropy"); 
         depth <- Array(5, 10, 20); 
         bins <- Array(50, 100, 200, 300)) yield {
-        val model = DecisionTree.trainClassifier(trainDt, numClasses, catFeature, impurity, depth, bins)
-        val predictionsAndLabels = valDt.map(example => (model.predict(example.features), example.label))
+        val model = DecisionTree.trainClassifier(trainingData, numClasses, catFeature, impurity, depth, bins)
+        val predictionsAndLabels = valData.map(example => (model.predict(example.features), example.label))
         val areaUnderPR = new BinaryClassificationMetrics(predictionsAndLabels).areaUnderPR
         ((impurity, depth, bins), areaUnderPR)
     }
@@ -128,13 +120,13 @@ for (x <- 0 to featuresVectorTrain.size-1) {
     val bestDTDepth = modelTuningDT.maxBy(_._2)._1._2
     val bestDTBins = modelTuningDT.maxBy(_._2)._1._3
 
-    val modelTuningRF = for ((trainDt, valDt) <- cvData;
+    val modelTuningRF = for ((trainingData, valData) <- cvData;
         trees <- Array(5, 10, 20); 
         impurity <- Array("gini", "entropy"); 
         depth <- Array(5, 10, 20); 
         bins <- Array(50, 100, 200, 300)) yield {
-        val model = RandomForest.trainClassifier(trainDt, numClasses, catFeature, trees, "auto", impurity, depth, bins)
-        val predictionsAndLabels = valDt.map(example => (model.predict(example.features), example.label))
+        val model = RandomForest.trainClassifier(trainingData, numClasses, catFeature, trees, "auto", impurity, depth, bins)
+        val predictionsAndLabels = valData.map(example => (model.predict(example.features), example.label))
         val areaUnderPR = new BinaryClassificationMetrics(predictionsAndLabels).areaUnderPR
         ((trees, impurity, depth, bins), areaUnderPR)
     }
@@ -148,9 +140,8 @@ for (x <- 0 to featuresVectorTrain.size-1) {
     val modelDT = DecisionTree.trainClassifier(trainingData, numClasses, catFeature, bestDTImpurity, bestDTDepth, bestDTBins)
     val modelRF = RandomForest.trainClassifier(trainingData, numClasses, catFeature, bestNuTrees, "auto", bestRFImpurity, bestRFDepth, bestRFBins)
     trainingData.unpersist()
-    allTestData.persist()
-    trainDt.unpersist()
-    valDt.unpersist()
+    valData.unpersist()
+    testData.persist()
 
     val predictionAndLabelsDT = testData.map { case LabeledPoint(label, features) =>
         val prediction = modelDT.predict(features)
@@ -161,7 +152,8 @@ for (x <- 0 to featuresVectorTrain.size-1) {
         val prediction = modelRF.predict(features)
         (prediction, label)
     }
-    allTestData.unpersist()
+
+    testData.unpersist()
     val metricsDT = new BinaryClassificationMetrics(predictionAndLabelsDT)
     val metricsRF = new BinaryClassificationMetrics(predictionAndLabelsRF)
 
